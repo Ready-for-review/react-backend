896a60c02a18db309af5fc65a6e999dc1ee21e9a
Daniel Mies <daniel.mies@codecentric.de>
2021-04-02 19:21:19 +0200

    static pikachu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search/>
+      <Pokemon/>
     </div>
   );
+}
+
+function Pokemon() {
+  return <>
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"/>
+          <h1>Pikachu</h1>
+         </>
+}
+
+function Search() {
+  return ""
 }
 
 export default App;
